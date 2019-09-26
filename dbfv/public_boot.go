36e588624e4a3f9b2552fe746875203e2f124815
Jean-Philippe Bossuat <jean-philippe.bossuat@epfl.ch>
2019-09-26 19:58:20 +0200

    Updated dbfv boot

--- dbfv/public_boot.go
+++ dbfv/public_boot.go
@@ -16,31 +16,35 @@ func GenBootShares(sk *bfv.SecretKey, ciphertext *bfv.Ciphertext, bfvcontext *bf
 
 	sampler := bfvcontext.ContextQ().NewKYSampler(3.19, 19)
 
-	mask := bfvcontext.ContextQ().NewUniformPoly()
+	polypool := bfvcontext.ContextQ().NewUniformPoly()
 	//mask := bfvcontext.NewPlaintext()
 	//encoder.EncodeUint(coeffs.Coeffs[0], mask)
 
 	// h0, h1 = e, e'
-	bootshares.h0 = sampler.SampleNTTNew()
-	bootshares.h1 = sampler.SampleNTTNew()
+	bootshares.h0 = bfvcontext.ContextQ().NewPoly() 
+	bootshares.h1 = bfvcontext.ContextQ().NewPoly() 
 
 	// h0 = e + mask
 	// h1 = e' - mask
-	bfvcontext.ContextQ().Add(bootshares.h0, mask, bootshares.h0)
-	bfvcontext.ContextQ().Sub(bootshares.h1, mask, bootshares.h1)
+	bfvcontext.ContextQ().Add(bootshares.h0, polypool, bootshares.h0)
+	bfvcontext.ContextQ().Sub(bootshares.h1, polypool, bootshares.h1)
 
 	// h0 = s*ct[1] - mask + e
-	bfvcontext.ContextQ().NTT(ciphertext.Value()[1], ciphertext.Value()[1])
-	bfvcontext.ContextQ().MulCoeffsMontgomeryAndAdd(sk.Get(), ciphertext.Value()[1], bootshares.h0)
-	bfvcontext.ContextQ().InvNTT(ciphertext.Value()[1], ciphertext.Value()[1])
+	bfvcontext.ContextQ().NTT(ciphertext.Value()[1], polypool)
+	bfvcontext.ContextQ().MulCoeffsMontgomeryAndAdd(sk.Get(), polypool, bootshares.h0)
 
 	// h1 = s*a + mask + e'
-	bfvcontext.ContextQ().NTT(crs, crs)
-	bfvcontext.ContextQ().MulCoeffsMontgomeryAndSub(sk.Get(), crs, bootshares.h1)
-	bfvcontext.ContextQ().InvNTT(crs, crs)
+	bfvcontext.ContextQ().NTT(crs, polypool)
+	bfvcontext.ContextQ().MulCoeffsMontgomeryAndSub(sk.Get(), polypool, bootshares.h1)
 
 	bfvcontext.ContextQ().InvNTT(bootshares.h0, bootshares.h0)
 	bfvcontext.ContextQ().InvNTT(bootshares.h1, bootshares.h1)
+
+	sampler.Sample(polypool)
+	bfvcontext.ContextQ().Add(bootshares.h0, polypool, bootshares.h0)
+
+	sampler.Sample(polypool)
+	bfvcontext.ContextQ().Add(bootshares.h1, polypool, bootshares.h1)
 
 	return
 }

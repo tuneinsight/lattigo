19b6f9fa3a5120ee134dbec3346db06ec258c567
Christian Grigis <christian.grigis@epfl.ch>
2019-10-30 17:45:36 +0100

    Cleaned up and improved doc on ckks example

--- examples/ckks/examples_ckks.go
+++ examples/ckks/examples_ckks.go
@@ -12,113 +12,114 @@ import (
 	"github.com/ldsec/lattigo/ckks"
 )
 
+// Random float in [min, max)
 func randomFloat(min, max float64) float64 {
 	return min + rand.Float64()*(max-min)
 }
 
-func randomComplex(min, max float64) complex128 {
-	return complex(randomFloat(min, max), randomFloat(min, max))
-}
-
-func chebyshevinterpolation() {
-
+func chebyshevInterpolation() {
 	// This example will pack random 8192 float64 values in the range [-8, 8] and approximate the function 1/(exp(-x) + 1) over the range [-8, 8].
 	// The result is then parsed and compared to the expected result.
 
 	rand.Seed(time.Now().UnixNano())
 
-	var err error
-
 	// Scheme params
 	params := ckks.DefaultParams[14]
 
 	// Context
-	var ckkscontext *ckks.CkksContext
-	if ckkscontext, err = ckks.NewCkksContext(params); err != nil {
+	ckksContext, err := ckks.NewCkksContext(params)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	encoder := ckkscontext.NewEncoder()
+	encoder := ckksContext.NewEncoder()
 
 	// Keys
-	kgen := ckkscontext.NewKeyGenerator()
-	var sk *ckks.SecretKey
-	var pk *ckks.PublicKey
-	sk, pk = kgen.NewKeyPair()
+	kgen := ckksContext.NewKeyGenerator()
+	sk, pk := kgen.NewKeyPair()
 
 	// Relinearization key
 	var rlk *ckks.EvaluationKey
-	if rlk, err = kgen.NewRelinKey(sk, ckkscontext.Scale()); err != nil {
+	if rlk, err = kgen.NewRelinKey(sk, ckksContext.Scale()); err != nil {
 		log.Fatal(err)
 	}
 
 	// Encryptor
-	var encryptor *ckks.Encryptor
-	if encryptor, err = ckkscontext.NewEncryptorFromPk(pk); err != nil {
+	encryptor, err := ckksContext.NewEncryptorFromPk(pk)
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Decryptor
-	var decryptor *ckks.Decryptor
-	if decryptor, err = ckkscontext.NewDecryptor(sk); err != nil {
+	decryptor, err := ckksContext.NewDecryptor(sk)
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Evaluator
-	var evaluator *ckks.Evaluator
-	if evaluator = ckkscontext.NewEvaluator(); err != nil {
-		log.Fatal(err)
-	}
+	evaluator := ckksContext.NewEvaluator() // Cannot return error?
 
 	// Values to encrypt
-	values := make([]complex128, ckkscontext.Slots())
+	values := make([]complex128, ckksContext.Slots())
 	for i := range values {
 		values[i] = complex(randomFloat(-8, 8), 0)
 	}
 
-	fmt.Printf("HEAAN parameters : logN = %d, logQ = %d, levels = %d, logScale = %d, sigma = %f \n", ckkscontext.LogN(), ckkscontext.LogQ(), ckkscontext.Levels(), ckkscontext.Scale(), ckkscontext.Sigma())
+	fmt.Printf("HEAAN parameters : logN = %d, logQ = %d, levels = %d, logScale = %d, sigma = %f\n",
+		ckksContext.LogN(), ckksContext.LogQ(), ckksContext.Levels(), ckksContext.Scale(), ckksContext.Sigma())
 
-	fmt.Println()
-	fmt.Printf("Values     : %6f %6f %6f %6f...\n", round(values[0]), round(values[1]), round(values[2]), round(values[3]))
-	fmt.Println()
+	fmt.Printf("\nValues     : %6f %6f %6f %6f...\n\n",
+		round(values[0]), round(values[1]), round(values[2]), round(values[3]))
 
-	// Plaintext creation and encoding process
-	plaintext := ckkscontext.NewPlaintext(ckkscontext.Levels()-1, ckkscontext.Scale())
-	if err = encoder.EncodeComplex(plaintext, values); err != nil {
+	// Plaintext creation
+	plaintext := ckksContext.NewPlaintext(ckksContext.Levels()-1, ckksContext.Scale())
+
+	// Encoding process
+	err = encoder.EncodeComplex(plaintext, values)
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Encryption process
-	var ciphertext *ckks.Ciphertext
-	if ciphertext, err = encryptor.EncryptNew(plaintext); err != nil {
+	ciphertext, err := encryptor.EncryptNew(plaintext)
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Evaluation of the function 1/(exp(-x)+1) in the range [-8, 8] (degree of approximation : 32)")
 
 	// Evaluation process
-	chebyapproximation := ckks.Approximate(f, -8, 8, 33) // We ask to approximate f(x) in the range [-8, 8] with a chebyshev polynomial of 33 coefficients (degree 32).
+	// We ask to approximate f(x) in the range [-8, 8] with a chebyshev polynomial of 33 coefficients (degree 32).
+	chebyApproximation := ckks.Approximate(f, -8, 8, 33)
 
-	if ciphertext, err = evaluator.EvaluateCheby(ciphertext, chebyapproximation, rlk); err != nil { // We evaluate the interpolated chebyshev polynomial on the ciphertext
+	// We evaluate the interpolated chebyshev polynomial on the ciphertext
+	resultCiphertext, err := evaluator.EvaluateCheby(ciphertext, chebyApproximation, rlk)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Done... Consumed levels :", ckkscontext.Levels()-1-ciphertext.Level())
+	fmt.Println("Done... Consumed levels :", ckksContext.Levels()-1-resultCiphertext.Level())
 
-	// Decryption process + Decoding process
-	valuesTest := encoder.DecodeComplex(decryptor.DecryptNew(ciphertext))
+	// Decryption process
+	resultPlaintext := decryptor.DecryptNew(resultCiphertext)
 
-	// Computation of the reference values
+	// Decoding process
+	computedValues := encoder.DecodeComplex(resultPlaintext)
+
+	// Compute the reference values
+	expectedValues := make([]complex128, len(values))
 	for i := range values {
-		values[i] = f(values[i])
+		expectedValues[i] = f(values[i])
 	}
 
 	// Printing results and comparison
 	fmt.Println()
-	fmt.Printf("ValuesTest : %6f %6f %6f %6f...\n", round(valuesTest[0]), round(valuesTest[1]), round(valuesTest[2]), round(valuesTest[3]))
-	fmt.Printf("ValuesWant : %6f %6f %6f %6f...\n", round(values[0]), round(values[1]), round(values[2]), round(values[3]))
-	verify_vector(values, valuesTest)
+	fmt.Printf("Computed values : %6f %6f %6f %6f...\n",
+		round(computedValues[0]), round(computedValues[1]), round(computedValues[2]), round(computedValues[3]))
+	fmt.Printf("Expected values : %6f %6f %6f %6f...\n",
+		round(expectedValues[0]), round(expectedValues[1]), round(expectedValues[2]), round(expectedValues[3]))
 
+	verifyVector(expectedValues, computedValues)
 }
 
 func f(x complex128) complex128 {
@@ -130,72 +131,65 @@ func round(x complex128) complex128 {
 	factor = 100000000
 	a := math.Round(real(x)*factor) / factor
 	b := math.Round(imag(x)*factor) / factor
+
 	return complex(a, b)
 }
 
-func verify_vector(valuesWant, valuesTest []complex128) (err error) {
-
+func verifyVector(expectedValues, computedValues []complex128) {
 	var deltaReal, deltaImag float64
+	var minPrec, maxPrec, meanPrec, medianPrec complex128
 
-	var minprec, maxprec, meanprec, medianprec complex128
+	diff := make([]complex128, len(expectedValues))
 
-	diff := make([]complex128, len(valuesWant))
+	minPrec = complex(0, 0)
+	maxPrec = complex(1, 1)
 
-	minprec = complex(0, 0)
-	maxprec = complex(1, 1)
+	meanPrec = complex(0, 0)
 
-	meanprec = complex(0, 0)
-
-	distrib_real := make(map[uint64]uint64)
-	distrib_imag := make(map[uint64]uint64)
-
-	for i := range valuesWant {
-
+	for i := range expectedValues {
 		// Test the ratio for big values (> 1) and difference for small values (< 1)
 
-		deltaReal = math.Abs(real(valuesTest[i]) - real(valuesWant[i]))
-		deltaImag = math.Abs(imag(valuesTest[i]) - imag(valuesWant[i]))
+		deltaReal = math.Abs(real(computedValues[i]) - real(expectedValues[i]))
+		deltaImag = math.Abs(imag(computedValues[i]) - imag(expectedValues[i]))
 
 		diff[i] += complex(deltaReal, 0)
 		diff[i] += complex(0, deltaImag)
 
-		meanprec += diff[i]
+		meanPrec += diff[i]
 
-		if real(diff[i]) > real(minprec) {
-			minprec = complex(real(diff[i]), 0)
+		if real(diff[i]) > real(minPrec) {
+			minPrec = complex(real(diff[i]), 0)
 		}
 
-		if imag(diff[i]) > imag(minprec) {
-			minprec = complex(real(minprec), imag(diff[i]))
+		if imag(diff[i]) > imag(minPrec) {
+			minPrec = complex(real(minPrec), imag(diff[i]))
 		}
 
-		if real(diff[i]) < real(maxprec) {
-			maxprec = complex(real(diff[i]), 0)
+		if real(diff[i]) < real(maxPrec) {
+			maxPrec = complex(real(diff[i]), 0)
 		}
 
-		if imag(diff[i]) < imag(maxprec) {
-			maxprec = complex(real(maxprec), imag(diff[i]))
+		if imag(diff[i]) < imag(maxPrec) {
+			maxPrec = complex(real(maxPrec), imag(diff[i]))
 		}
-
-		distrib_real[uint64(math.Floor(math.Log2(1/real(diff[i]))))] += 1
-		distrib_imag[uint64(math.Floor(math.Log2(1/imag(diff[i]))))] += 1
 	}
 
-	meanprec /= complex(float64(len(valuesWant)), 0)
-	medianprec = calcmedian(diff)
+	meanPrec /= complex(float64(len(expectedValues)), 0)
+	medianPrec = computeMedian(diff)
 
 	fmt.Println()
-	fmt.Printf("Minimum precision : (%.2f, %.2f) bits \n", math.Log2(1/real(minprec)), math.Log2(1/imag(minprec)))
-	fmt.Printf("Maximum precision : (%.2f, %.2f) bits \n", math.Log2(1/real(maxprec)), math.Log2(1/imag(maxprec)))
-	fmt.Printf("Mean    precision : (%.2f, %.2f) bits \n", math.Log2(1/real(meanprec)), math.Log2(1/imag(meanprec)))
-	fmt.Printf("Median  precision : (%.2f, %.2f) bits \n", math.Log2(1/real(medianprec)), math.Log2(1/imag(medianprec)))
+	fmt.Printf("Minimum precision : (%.2f, %.2f) bits\n",
+		math.Log2(1/real(minPrec)), math.Log2(1/imag(minPrec)))
+	fmt.Printf("Maximum precision : (%.2f, %.2f) bits\n",
+		math.Log2(1/real(maxPrec)), math.Log2(1/imag(maxPrec)))
+	fmt.Printf("Mean    precision : (%.2f, %.2f) bits\n",
+		math.Log2(1/real(meanPrec)), math.Log2(1/imag(meanPrec)))
+	fmt.Printf("Median  precision : (%.2f, %.2f) bits\n",
+		math.Log2(1/real(medianPrec)), math.Log2(1/imag(medianPrec)))
 	fmt.Println()
-
-	return nil
 }
 
-func calcmedian(values []complex128) (median complex128) {
-
+func computeMedian(values []complex128) (median complex128) {
 	tmp := make([]float64, len(values))
 
 	for i := range values {
@@ -225,9 +219,8 @@ func calcmedian(values []complex128) (median complex128) {
 	}
 
 	return (values[index] + values[index+1]) / 2
-
 }
 
 func main() {
-	chebyshevinterpolation()
+	chebyshevInterpolation()
 }

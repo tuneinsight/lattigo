47252cb2dbf5389ae0f9ddc68c2cd494bab42607
Christian M <christian.mouchet@epfl.ch>
2020-10-09 13:35:16 +0200

    added a root package

---
+++ lattigo.go
@@ -0,0 +1,10 @@
+/*
+Package lattigo is a cryptographic library implementing lattice-based cryptographic primitives. The library features:
+
+    - A pure Go implementation enabling code-simplicity and easy builds.
+    - A public interface for an efficient multi-precision polynomial arithmetic layer.
+    - Comparable performance to state-of-the-art C++ libraries.
+
+Lattigo aims at enabling fast prototyping of secure-multiparty computation solutions based on distributed homomorphic cryptosystems, by harnessing Go's natural concurrency model.
+*/
+package lattigo

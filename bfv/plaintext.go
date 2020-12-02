89576e4452b12f447046cd4384f229eff09ad47e
Jean-Philippe Bossuat <jean-philippe.bossuat@epfl.ch>
2020-12-02 09:47:43 +0100

    [bfv] : documented the plaintext changes

--- bfv/plaintext.go
+++ bfv/plaintext.go
@@ -12,6 +12,10 @@ type Plaintext struct {
 
 // NewPlaintextZQ creates and allocates a new plaintext in ZQ (multiple moduli of Q).
 // The plaintext will be in ZQ and scaled by Q/t.
+// Faster encryption
+// Slower encoding and larger plaintext size
+// Faster ct + pt
+// Cannot do ct * pt
 func NewPlaintextZQ(params *Parameters) *Plaintext {
 	plaintext := &Plaintext{NewElement(params, 0, true), nil}
 	plaintext.value = plaintext.Element.value[0]
@@ -22,6 +26,10 @@ func NewPlaintextZQ(params *Parameters) *Plaintext {
 
 // NewPlaintextZT creates and allocates a new plaintext in ZT (single modulus T).
 // The plaintext will be in Zt.
+// Faster encoding + smaller plaintext size
+// Slower encryption
+// Slower ct + pt
+// Cannot do ct * pt
 func NewPlaintextZT(params *Parameters) *Plaintext {
 
 	plaintext := &Plaintext{NewElement(params, 0, false), nil}
@@ -33,6 +41,8 @@ func NewPlaintextZT(params *Parameters) *Plaintext {
 
 // NewPlaintextMul creates and allocates a new plaintext optimized for ciphertext x plaintext multiplication.
 // The plaintext will be in the NTT and Montgomery domain of ZQ and not scaled by Q/t.
+// Cannot do ct + pt
+// Fast ct * pt
 func NewPlaintextMul(params *Parameters) *Plaintext {
 	plaintext := &Plaintext{NewElement(params, 0, true), nil}
 	plaintext.value = plaintext.Element.value[0]

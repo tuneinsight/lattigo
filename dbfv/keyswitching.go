5a955efb17e3ec1e190b8a167dac3f5a2a6d7cd5
Christian <christian.mouchet@bluewin.ch>
2021-05-04 15:42:21 +0200

    renamed public_permute.go -> masked_transform.go

--- dbfv/keyswitching.go
+++ dbfv/keyswitching.go
@@ -16,18 +16,3 @@ type CKSProtocol struct {
 func NewCKSProtocol(params bfv.Parameters, sigmaSmudging float64) *CKSProtocol {
 	return &CKSProtocol{*drlwe.NewCKSProtocol(params.Parameters, sigmaSmudging)}
 }
-
-// // GenShare is the first and unique round of the CKSProtocol protocol. Each party holding a ciphertext ctx encrypted under a collective publick-key musth
-// // compute the following :
-// //
-// // [(skInput_i - skOutput_i) * ctx[0] + e_i]
-// //
-// // Each party then broadcast the result of this computation to the other j-1 parties.
-// func (cks *CKSProtocol) GenShare(skInput, skOutput *rlwe.SecretKey, ct *bfv.Ciphertext, shareOut drlwe.CKSShare) {
-// 	cks.CKSProtocol.GenShare(skInput, skOutput, ct, shareOut)
-// }
-
-// KeySwitch performs the actual keyswitching operation on a ciphertext ct and put the result in ctOut
-// func (cks *CKSProtocol) KeySwitch(combined drlwe.CKSShare, ct *bfv.Ciphertext, ctOut *bfv.Ciphertext) {
-// 	cks.CKSProtocol.KeySwitch(combined, ct, ctOut)
-// }

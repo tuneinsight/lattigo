d96f131dcfd34d7ece1ad451cb39f0e31dd822aa
Christian <christian.mouchet@bluewin.ch>
2021-06-07 17:15:45 +0200

    refined some API methods of s2e,e2s and refresh + some missing godoc

--- dbfv/shares_to_enc.go
+++ dbfv/shares_to_enc.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ldsec/lattigo/v2/rlwe"
 )
 
+// S2EProtocol is the structure storing the parameters and temporary buffers
+// required by the shares-to-encryption protocol.
 type S2EProtocol struct {
 	CKSProtocol
 
@@ -17,6 +19,7 @@ type S2EProtocol struct {
 	tmpPlaintext *bfv.Plaintext
 }
 
+// NewS2EProtocol creates a new S2EProtocol struct from the passed BFV parameters.
 func NewS2EProtocol(params bfv.Parameters, sigmaSmudging float64) *S2EProtocol {
 	e2s := new(S2EProtocol)
 	e2s.CKSProtocol = *NewCKSProtocol(params, sigmaSmudging)
@@ -27,13 +30,17 @@ func NewS2EProtocol(params bfv.Parameters, sigmaSmudging float64) *S2EProtocol {
 	return e2s
 }
 
-func (s2e *S2EProtocol) GenShare(sk *rlwe.SecretKey, c1 *ring.Poly, secretShare AdditiveShare, c0ShareOut *drlwe.CKSShare) {
+// GenShare generates a party's in the shares-to-encryption protocol given the party's secret-key share `sk`, a common
+// polynomial sampled from the CRS `c1` and the party's secret share of the message.
+func (s2e *S2EProtocol) GenShare(sk *rlwe.SecretKey, c1 *ring.Poly, secretShare rlwe.AdditiveShare, c0ShareOut *drlwe.CKSShare) {
 	s2e.encoder.ScaleUp(&bfv.PlaintextRingT{Plaintext: &rlwe.Plaintext{Value: &secretShare.Value}}, s2e.tmpPlaintext)
 	s2e.CKSProtocol.GenShare(s2e.zero, sk, &rlwe.Element{Value: []*ring.Poly{c0ShareOut.Value, c1}}, c0ShareOut)
 	s2e.ringQ.Add(c0ShareOut.Value, s2e.tmpPlaintext.Value, c0ShareOut.Value)
 }
 
-func (s2e *S2EProtocol) Finalize(c0Agg *drlwe.CKSShare, c1 *ring.Poly, ctOut bfv.Ciphertext) {
+// GetEncryption computes the final encryption of the secret-shared message when provided with the aggregation `c0Agg` of the parties'
+// share in the protocol and with the common, CRS-sampled polynomial `c1`.
+func (s2e *S2EProtocol) GetEncryption(c0Agg *drlwe.CKSShare, c1 *ring.Poly, ctOut bfv.Ciphertext) {
 	if ctOut.Degree() != 1 {
 		panic("ctOut must have degree 1.")
 	}

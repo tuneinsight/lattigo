8debb2e7a2da725c510371dd73b9d948c152e186
Christian M <christian.mouchet@epfl.ch>
2021-07-12 12:04:21 +0200

    reduced the rlwe.Encryptor public API

--- ckks/encryptor.go
+++ ckks/encryptor.go
@@ -7,42 +7,36 @@ import (
 
 // Encryptor an interface wrapping an rlwe.Encryptor.
 type Encryptor interface {
-	EncryptNew(plaintext *Plaintext) *Ciphertext
 	Encrypt(plaintext *Plaintext, ciphertext *Ciphertext)
-	EncryptFastNew(plaintext *Plaintext) *Ciphertext
-	EncryptFast(plaintext *Plaintext, ciphertext *Ciphertext)
+	EncryptNew(plaintext *Plaintext) *Ciphertext
 	EncryptFromCRP(plaintext *Plaintext, ciphertext *Ciphertext, crp *ring.Poly)
 	EncryptFromCRPNew(plaintext *Plaintext, crp *ring.Poly) *Ciphertext
 }
 
 type encryptor struct {
 	rlwe.Encryptor
+	params Parameters
 }
 
 // NewEncryptor instanciate a new wrapped rlwe.Encryptor for CKKS ciphertexts and plaintexts.
 func NewEncryptor(params Parameters, key interface{}) Encryptor {
-	return &encryptor{rlwe.NewEncryptor(params.Parameters, key)}
+	return &encryptor{rlwe.NewEncryptor(params.Parameters, key), params}
+}
+
+func NewFastEncryptor(params Parameters, key *rlwe.PublicKey) Encryptor {
+	return &encryptor{rlwe.NewFastEncryptor(params.Parameters, key), params}
 }
 
 // EncryptNew calls rlwe.Encryptor.EncryptNew.
 func (enc *encryptor) EncryptNew(plaintext *Plaintext) *Ciphertext {
-	return &Ciphertext{Element: enc.Encryptor.EncryptNTTNew(&rlwe.Plaintext{Value: plaintext.Value}), Scale: plaintext.Scale}
+	ct := NewCiphertext(enc.params, 1, plaintext.Level(), plaintext.Scale)
+	enc.Encryptor.Encrypt(&rlwe.Plaintext{Value: plaintext.Value}, ct.Element)
+	return ct
 }
 
 // Encrypt calls rlwe.Encryptor.Encrypt.
 func (enc *encryptor) Encrypt(plaintext *Plaintext, ciphertext *Ciphertext) {
 	enc.Encryptor.Encrypt(&rlwe.Plaintext{Value: plaintext.Value}, &rlwe.Element{Value: ciphertext.Value})
-	ciphertext.Scale = plaintext.Scale
-}
-
-// EncryptFastNew calls rlwe.Encryptor.EncryptFastNew.
-func (enc *encryptor) EncryptFastNew(plaintext *Plaintext) *Ciphertext {
-	return &Ciphertext{Element: enc.Encryptor.EncryptFastNTTNew(&rlwe.Plaintext{Value: plaintext.Value}), Scale: plaintext.Scale}
-}
-
-// EncryptFast calls rlwe.Encryptor.EncryptFast.
-func (enc *encryptor) EncryptFast(plaintext *Plaintext, ciphertext *Ciphertext) {
-	enc.Encryptor.EncryptFast(&rlwe.Plaintext{Value: plaintext.Value}, &rlwe.Element{Value: ciphertext.Value})
 	ciphertext.Scale = plaintext.Scale
 }
 
@@ -54,5 +48,7 @@ func (enc *encryptor) EncryptFromCRP(plaintext *Plaintext, ciphertext *Ciphertex
 
 // EncryptFromCRPNew calls rlwe.Encryptor.EncryptFromCRPNew.
 func (enc *encryptor) EncryptFromCRPNew(plaintext *Plaintext, crp *ring.Poly) *Ciphertext {
-	return &Ciphertext{Element: enc.Encryptor.EncryptFromCRPNTTNew(&rlwe.Plaintext{Value: plaintext.Value}, crp), Scale: plaintext.Scale}
+	ct := NewCiphertext(enc.params, 1, plaintext.Level(), plaintext.Scale)
+	enc.Encryptor.EncryptFromCRP(&rlwe.Plaintext{Value: plaintext.Value}, ct.Element, crp)
+	return &Ciphertext{Element: ct.Element, Scale: plaintext.Scale}
 }

8b1ac7d6c88f980c5886c66c1941831dcc3453ee
Christian M <christian.mouchet@epfl.ch>
2021-07-12 18:46:05 +0200

    updated godoc

--- bfv/keygen.go
+++ bfv/keygen.go
@@ -2,6 +2,7 @@ package bfv
 
 import "github.com/ldsec/lattigo/v2/rlwe"
 
+// NewKeyGenerator creates a rlwe.KeyGenerator instance from the BFV parameters.
 func NewKeyGenerator(params Parameters) rlwe.KeyGenerator {
 	return rlwe.NewKeyGenerator(params.Parameters)
 }

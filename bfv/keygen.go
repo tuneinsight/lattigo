33168b0adffd9ef7679c4de3424da6f551e41b62
Christian <christian.mouchet@bluewin.ch>
2021-07-13 16:27:29 +0200

    reorganised the schemes' package structure

--- bfv/keygen.go
+++
@@ -1,8 +0,0 @@
-package bfv
-
-import "github.com/ldsec/lattigo/v2/rlwe"
-
-// NewKeyGenerator creates a rlwe.KeyGenerator instance from the BFV parameters.
-func NewKeyGenerator(params Parameters) rlwe.KeyGenerator {
-	return rlwe.NewKeyGenerator(params.Parameters)
-}

875384d27d49e1a908fe6f5ed2fb2980e0f76075
Christian <christian.mouchet@bluewin.ch>
2021-11-26 12:00:51 +0100

    typo in the ckks.Encoder godoc and minor phrasing improvements

--- ckks/encoder.go
+++ ckks/encoder.go
@@ -18,20 +18,21 @@ const GaloisGen uint64 = 5
 
 var pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989"
 
-// Encoder is an interface implementing the encoding and decoding operations. It provides method to embed []complex128 and []float64 types
-// into plaintexts and the inverse operations.
+// Encoder is an interface implementing the encoding and decoding operations. It provides methods to encode/decode []complex128 and []float64 types
+// into/from Plaintext types.
 // Two different encodings are provided:
-//
-//     - Slots: The coefficients are first subjected to a special Fourier transform before being embedded on the plaintext. This encoding can embed
-//              []complex128 and []float64 slices of size at most N/2 (N being the ring degree) and leverages the convolution property of the DFT
-//              to preserve point-wise complex multiplication in the plaintext domain, i.e. a ciphertext multiplication will result in an element-
-//              wise multiplication in the plaintext domain. It also enables plaintext slots cyclic rotations. Other operations, like addition or
-//              constant multiplication behave as usual.
 //
 //     - Coeffs: The coefficients are directly embedded on the plaintext. This encoding only allows to encode []float64 slices, but of size up to N
 //               (N being the ring degree) and does not preserve the point-wise multiplication. A ciphertext multiplication will result in a nega-
 //               cyclic polynomial convolution in the plaintext domain. This encoding does not provide native slot cyclic rotation.
 //               Other operations, like addition or constant multiplication behave as usual.
+//
+//     - Slots: The coefficients are first subjected to a special Fourier transform before being embedded in the plaintext by using Coeffs encoding.
+//              This encoding can embed []complex128 and []float64 slices of size at most N/2 (N being the ring degree) and leverages the convolution
+//              property of the DFT to preserve point-wise complex multiplication in the plaintext domain, i.e. a ciphertext multiplication will result
+//              in an element-wise multiplication in the plaintext domain. It also enables plaintext slots cyclic rotations. Other operations, like
+//              constant multiplication behave as usual. It is condidered the default encoding method for CKKS.
+//
 //
 // The figure bellow illustrates the relationship between those two encoding:
 //

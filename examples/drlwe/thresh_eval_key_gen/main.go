5a23ade02d91d18b92798b1cb4d52f4b7c5e6562
Christian Mouchet <christian.mouchet@epfl.ch>
2022-08-29 15:55:44 +0200

    documented drlwe example

--- examples/drlwe/thresh_eval_key_gen/main.go
+++ examples/drlwe/thresh_eval_key_gen/main.go
@@ -14,17 +14,25 @@ import (
 	"github.com/tuneinsight/lattigo/v3/utils"
 )
 
-type genTask struct {
-	group     []*party
-	galoisEls []uint64
-}
+// This example showcases the use of the drlwe package to generate an evaluation key in a multiparty setting.
+// It simulate multiple parties and their interactions within a single Go program using multiple goroutines.
+// The parties use the t-out-of-N-threshold RLWE encryption scheme as described in "An Efficient Threshold
+// Access-Structure for RLWE-Based Multiparty Homomorphic Encryption" (2022) by Mouchet, C., Bertrand, E. and
+// Hubaux, J. P. (https://eprint.iacr.org/2022/780). Moreover, this scenario showcases the use of a cloud
+// server that assists the parties in the execution of the protocol, by collecting and aggregating their public
+// shares.
+//
+// The scenario can be parameterized by the program arguments. Notably:
+// 	- the total number of parties,
+//  - the corruption threshold, as the number of guaranteed honest parties,
+//  - the number of parties being online to generate the evaluation key,
+//  - the parameters of the RLWE cryptosystem for which the evaluation-key is generated
+//  - the size of the evaluation-key to be generated, as the number of switching keys.
+//
+// If the number of online parties is greater than the threshold, the scenario simulates the distribution of the
+// workload among the set of online parties.
 
-type genTaskResult struct {
-	galEl uint64
-
-	rtgShare *drlwe.RTGShare
-}
-
+// party represents a party in the scenario.
 type party struct {
 	*drlwe.RTGProtocol
 	*drlwe.Thresholdizer
@@ -39,10 +47,7 @@ type party struct {
 	genTaskQueue chan genTask
 }
 
-func (p *party) String() string {
-	return fmt.Sprintf("Party#%d", p.i)
-}
-
+// cloud represents the cloud server assisting the parties.
 type cloud struct {
 	*drlwe.RTGProtocol
 
@@ -53,8 +58,11 @@ type cloud struct {
 	}
 }
 
-var crp drlwe.RTGCRP
+var crp map[uint64]drlwe.RTGCRP
 
+// Run simulate the behavior of a party during the key generation protocol. The parties process
+// a queue of share-generation tasks which is attributed to them by a protocol orchestrator
+// (simulated in this example).
 func (p *party) Run(wg *sync.WaitGroup, params rlwe.Parameters, N int, P []*party, C *cloud) {
 
 	var nShares, nTasks int
@@ -80,7 +88,7 @@ func (p *party) Run(wg *sync.WaitGroup, params rlwe.Parameters, N int, P []*part
 		for _, galEl := range task.galoisEls {
 			rtgShare := p.AllocateShare()
 
-			p.GenShare(sk, galEl, crp, rtgShare)
+			p.GenShare(sk, galEl, crp[galEl], rtgShare)
 			C.aggTaskQueue <- genTaskResult{galEl: galEl, rtgShare: rtgShare}
 			nShares++
 			byteSent += len(rtgShare.Value) * len(rtgShare.Value[0]) * rtgShare.Value[0][0].GetDataLen64(false)
@@ -92,6 +100,13 @@ func (p *party) Run(wg *sync.WaitGroup, params rlwe.Parameters, N int, P []*part
 	fmt.Printf("\tParty %d finished generating %d shares of %d tasks in %s, sent %s\n", p.i, nShares, nTasks, cpuTime, formatByteSize(byteSent))
 }
 
+func (p *party) String() string {
+	return fmt.Sprintf("Party#%d", p.i)
+}
+
+// Run simulate the behavior of the cloud during the key generation protocol.
+// The cloud process aggregation requests and generates the switching keys when
+// all the parties' shares have been aggregated.
 func (c *cloud) Run(galEls []uint64, params rlwe.Parameters, t int) {
 
 	shares := make(map[uint64]*struct {
@@ -115,7 +130,7 @@ func (c *cloud) Run(galEls []uint64, params rlwe.Parameters, t int) {
 		acc.needed--
 		if acc.needed == 0 {
 			rtk := rlwe.NewSwitchingKey(params, params.MaxLevel(), params.PCount()-1)
-			c.GenRotationKey(acc.share, crp, rtk)
+			c.GenRotationKey(acc.share, crp[task.galEl], rtk)
 			c.finDone <- struct {
 				galEl uint64
 				rtk   rlwe.SwitchingKey
@@ -128,45 +143,6 @@ func (c *cloud) Run(galEls []uint64, params rlwe.Parameters, t int) {
 	close(c.finDone)
 	fmt.Printf("\tCloud finished aggregating %d shares in %s, received %s\n", i, cpuTime, formatByteSize(byteRecv))
 
-}
-
-func getSubGroups(P []*party, t, k int) [][]*party {
-	if t == len(P) {
-		return [][]*party{P}
-	}
-	if t > len(P) {
-		panic("t > len(P)")
-	}
-
-	groups := [][]*party{}
-	for i := 0; i < k; i++ {
-		start := (i * t) % len(P)
-		end := ((i + 1) * t) % len(P)
-		switch {
-		case i > 0 && start == 0 && end == t:
-			return groups
-		case start > end:
-			group := make([]*party, t)
-			copy(group, P[0:end])
-			copy(group[end:], P[start:])
-			groups = append(groups, group)
-		default:
-			groups = append(groups, P[start:end])
-		}
-	}
-
-	return groups
-}
-
-func getTasks(galEls []uint64, groups [][]*party) []genTask {
-	tasks := make([]genTask, len(groups))
-	for i := range tasks {
-		tasks[i].group = groups[i]
-	}
-	for i, galEl := range galEls {
-		tasks[i%len(groups)].galoisEls = append(tasks[i%len(groups)].galoisEls, galEl)
-	}
-	return tasks
 }
 
 var flagN = flag.Int("N", 3, "the number of parties")
@@ -243,32 +219,43 @@ func main() {
 			rtk   rlwe.SwitchingKey
 		}, len(galEls)),
 	}
+
+	// Initialize the parties' state
 	P := make([]*party, N)
 	skIdeal := rlwe.NewSecretKey(params)
 	shamirPks := make([]drlwe.ShamirPublicPoint, 0)
+
 	for i := range P {
 		pi := new(party)
 		pi.RTGProtocol = drlwe.NewRTGProtocol(params)
-		pi.Thresholdizer = drlwe.NewThresholdizer(params)
 		pi.i = i
 		pi.sk = kg.GenSecretKey()
-		params.RingQP().AddLvl(params.QCount()-1, params.PCount()-1, skIdeal.Value, pi.sk.Value, skIdeal.Value)
-		pi.tsk = pi.AllocateThresholdSecretShare()
-		pi.ssp, err = pi.GenShamirPolynomial(t, pi.sk)
-		if err != nil {
-			panic(err)
-		}
-		pi.shamirPk = drlwe.ShamirPublicPoint(i + 1)
-		shamirPks = append(shamirPks, pi.shamirPk)
 		pi.genTaskQueue = make(chan genTask, k)
+
+		if t != N {
+			pi.Thresholdizer = drlwe.NewThresholdizer(params)
+			pi.tsk = pi.AllocateThresholdSecretShare()
+			pi.ssp, err = pi.GenShamirPolynomial(t, pi.sk)
+			if err != nil {
+				panic(err)
+			}
+			pi.shamirPk = drlwe.ShamirPublicPoint(i + 1)
+		}
+
 		P[i] = pi
+
+		// computes the ideal sk for the sake of the example
+		params.RingQP().AddLvl(params.QCount()-1, params.PCount()-1, skIdeal.Value, pi.sk.Value, skIdeal.Value)
+
+		shamirPks = append(shamirPks, pi.shamirPk)
 	}
 
-	for _, pi := range P {
-		pi.Combiner = drlwe.NewCombiner(params, pi.shamirPk, shamirPks, t)
-	}
+	// if t < N, use the t-out-of-N scheme and performs the share-resharing procedure.
+	if t != N {
+		for _, pi := range P {
+			pi.Combiner = drlwe.NewCombiner(params, pi.shamirPk, shamirPks, t)
+		}
 
-	if t < N {
 		fmt.Println("Performing threshold setup")
 		shares := make(map[*party]map[*party]*drlwe.ShamirSecretShare, len(P))
 		for _, pi := range P {
@@ -288,20 +275,29 @@ func main() {
 		}
 	}
 
-	P = P[:o]
+	P = P[:o] // reduce the set of parties to the online ones.
 
 	groups := getSubGroups(P, t, k)
 	fmt.Printf("Generating %d rotation keys with %d parties in %d groups\n", len(galEls), len(P), len(groups))
 
-	crp = P[0].SampleCRP(crs)
+	// Sample the common random polynomials from the CRS.
+	// For the scenario, we consider it is provided as-is to the parties.
+	crp = make(map[uint64]drlwe.RTGCRP)
+	for _, galEl := range galEls {
+		crp[galEl] = P[0].SampleCRP(crs)
+	}
 
+	// Start the cloud and the parties
 	go C.Run(galEls, params, t)
 	for _, pi := range P {
 		go pi.Run(wg, params, N, P, C)
 	}
-
 	wg.Add(len(P))
 	start := time.Now()
+
+	// distribute the key generation sub-tasks among the online parties. This
+	// simulates a protocol orchestrator affecting each party with the tasks
+	// of generating specific switching keys.
 	tasks := getTasks(galEls, groups)
 	for _, task := range tasks {
 		for _, p := range task.group {
@@ -314,6 +310,7 @@ func main() {
 	wg.Wait()
 	close(C.aggTaskQueue)
 
+	// collects the results
 	rtks := make(map[uint64]rlwe.SwitchingKey)
 	for task := range C.finDone {
 		rtks[task.galEl] = task.rtk
@@ -332,6 +329,56 @@ func main() {
 		}
 	}
 	fmt.Println("done")
+}
+
+type genTask struct {
+	group     []*party
+	galoisEls []uint64
+}
+
+type genTaskResult struct {
+	galEl uint64
+
+	rtgShare *drlwe.RTGShare
+}
+
+func getTasks(galEls []uint64, groups [][]*party) []genTask {
+	tasks := make([]genTask, len(groups))
+	for i := range tasks {
+		tasks[i].group = groups[i]
+	}
+	for i, galEl := range galEls {
+		tasks[i%len(groups)].galoisEls = append(tasks[i%len(groups)].galoisEls, galEl)
+	}
+	return tasks
+}
+
+func getSubGroups(P []*party, t, k int) [][]*party {
+	if t == len(P) {
+		return [][]*party{P}
+	}
+	if t > len(P) {
+		panic("t > len(P)")
+	}
+
+	groups := [][]*party{}
+	for i := 0; i < k; i++ {
+		start := (i * t) % len(P)
+		end := ((i + 1) * t) % len(P)
+		switch {
+		case i > 0 && start == 0 && end == t:
+			return groups
+		case start > end:
+			group := make([]*party, t)
+			copy(group, P[0:end])
+			copy(group[end:], P[start:])
+			groups = append(groups, group)
+		default:
+			groups = append(groups, P[start:end])
+		}
+	}
+
+	return groups
 }
 
 func formatByteSize(b int) string {

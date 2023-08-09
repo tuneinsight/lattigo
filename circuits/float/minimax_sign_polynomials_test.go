4d58d7591bc30240f33b43f07c3d367fddb53f0a
Jean-Philippe Bossuat <jean-philippe@tuneinsight.com>
2023-08-10 00:30:09 +0200

    [circuits/float]: new sign poly

--- circuits/float/minimax_sign_polynomials_test.go
+++ circuits/float/minimax_sign_polynomials_test.go
@@ -1,7 +1,7 @@
 package float
 
 import (
-	//"fmt"
+	"fmt"
 	"math"
 	"sort"
 	"testing"
@@ -16,10 +16,10 @@ func TestMinimaxApprox(t *testing.T) {
 	prec := uint(512)
 
 	// 2^{-logalpha} distinguishing ability
-	logalpha := int(30)
+	logalpha := int(10)
 
 	// Degrees of each minimax polynomial
-	deg := []int{16, 16, 16, 32, 32, 32, 32, 32}
+	deg := []int{8, 8, 18, 32}
 
 	GenSignPoly(prec, logalpha, deg)
 }
@@ -55,5 +55,40 @@ func TestMinimaxCompositeSignPolys30bits(t *testing.T) {
 
 		require.Greater(t, -30.0, math.Log2(1-xPosF64))
 		require.Greater(t, -30.0, math.Log2(1+xNegF64))
+	}
+}
+
+func TestMinimaxCompositeSignPolys20bits(t *testing.T) {
+
+	keys := make([]int, len(SingPoly20String))
+
+	idx := 0
+	for k := range SingPoly20String {
+		keys[idx] = k
+		idx++
+	}
+
+	sort.Ints(keys)
+
+	for _, alpha := range keys[:] {
+
+		polys, err := GetSignPoly20Polynomials(alpha)
+		require.NoError(t, err)
+
+		xPos := bignum.NewFloat(math.Exp2(-float64(alpha)), 53)
+		xNeg := bignum.NewFloat(-math.Exp2(-float64(alpha)), 53)
+
+		for _, poly := range polys {
+			xPos = poly.Evaluate(xPos)[0]
+			xNeg = poly.Evaluate(xNeg)[0]
+		}
+
+		xPosF64, _ := xPos.Float64()
+		xNegF64, _ := xNeg.Float64()
+
+		fmt.Println(alpha, math.Log2(1-xPosF64), math.Log2(1+xNegF64))
+
+		require.Greater(t, -20.0, math.Log2(1-xPosF64))
+		require.Greater(t, -20.0, math.Log2(1+xNegF64))
 	}
 }

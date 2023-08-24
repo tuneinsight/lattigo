98b412dc0e50068352e9b82b5890896d4b2a5af0
Jean-Philippe Bossuat <jean-philippe@tuneinsight.com>
2023-08-24 12:04:03 +0200

    temporary fix for v1.21: https://github.com/golang/go/issues/61992

--- rlwe/gadgetciphertext.go
+++ rlwe/gadgetciphertext.go
@@ -229,8 +229,11 @@ func AddPolyTimesGadgetVectorToGadgetCiphertext(pt ring.Poly, cts []GadgetCipher
 			}
 		}
 
+		// Temporary fix of compiler error https://github.com/golang/go/issues/61992
+		tmpfix := uint64(1 << cts[0].BaseTwoDecomposition)
+
 		// w^2j
-		ringQ.MulScalar(buff, 1<<cts[0].BaseTwoDecomposition, buff)
+		ringQ.MulScalar(buff, tmpfix, buff)
 	}
 
 	return
@@ -287,7 +290,9 @@ func NewGadgetPlaintext(params Parameters, value interface{}, levelQ, levelP, ba
 		pt.Value[i] = *pt.Value[0].CopyNew()
 
 		for j := 0; j < i; j++ {
-			ringQ.MulScalar(pt.Value[i], 1<<baseTwoDecomposition, pt.Value[i])
+			// Temporary fix of compiler error https://github.com/golang/go/issues/61992
+			tmpfix := uint64(1 << baseTwoDecomposition)
+			ringQ.MulScalar(pt.Value[i], tmpfix, pt.Value[i])
 		}
 	}
 

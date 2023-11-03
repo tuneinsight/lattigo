6a953d9f59d0bc7442dc70d46816b78912707249
Jean-Philippe <boss_ldmjp@hotmail.com>
2023-11-03 16:47:29 +0100

    wip

--- schemes/schemes.go
+++
@@ -1,4 +0,0 @@
-// Package schemes implement Ring-Learning-With-Errors-based Homomorphic Encryption schemes.
-// This package is imported by the package `he` which abstract the schemes and provides
-// Homomorphic Encryption based on the plaintext domain and functionalities.
-package schemes
